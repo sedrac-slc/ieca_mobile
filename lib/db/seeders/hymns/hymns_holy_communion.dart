c9821a79a2618751b8e90563bba8ed633e3fc9c6
sedrac-slc <slcsedrac@gmail.com>
2025-06-14 09:03:31 +0100

    add line-breaks

--- lib/db/seeders/hymns/hymns_holy_communion.dart
+++ lib/db/seeders/hymns/hymns_holy_communion.dart
@@ -5,7 +5,7 @@ import 'package:ieca_mobile/models/_import.dart';
 class HymnsHolyCommunion{
   static List<HymnsContent> list = [
 
-    HymnsContent(id: 438, position: 1, content: "Aqui reunidos, vemos o Senhor; Aqui sentados, vemos Deus dos Céus, E prometemos toda confiança; Aqui pensamos Deus em todo ser.", hymnsNumber: HymnsNumberSeeder.HOLY_COMMUNION_105, typeStanza: HymnsContentCode.VERSE),
+    HymnsContent(id: 438, position: 1, content: "Aqui reunidos, vemos o Senhor;\nAqui sentados, vemos Deus dos Céus,\nE prometemos toda confiança;\nAqui pensamos Deus em todo ser.", hymnsNumber: HymnsNumberSeeder.HOLY_COMMUNION_105, typeStanza: HymnsContentCode.VERSE),
     HymnsContent(id: 439, position: 2, content: "O Pão da vida comemos aqui;\nAqui bebemos da vide dos céus;\nAqui a bênção da vida fruir,\nE o perdão de Cristo receber.", hymnsNumber: HymnsNumberSeeder.HOLY_COMMUNION_105, typeStanza: HymnsContentCode.VERSE),
     HymnsContent(id: 440, position: 3, content: "Aqui, despida toda agonia,\nFortalecidos com poder de Deus;\nQue nos é dado pelo seu perdão.\nPois, sacrifício por nós padeceu.", hymnsNumber: HymnsNumberSeeder.HOLY_COMMUNION_105, typeStanza: HymnsContentCode.VERSE),
     HymnsContent(id: 441, position: 4, content: "Cristo é justiça que nos libertou;\nDas impurezas nos santificou;\nDe vestes brancas, Ele nos vestiu,\nP’ra sermos coerdeiros lá nos céus.", hymnsNumber: HymnsNumberSeeder.HOLY_COMMUNION_105, typeStanza: HymnsContentCode.VERSE),

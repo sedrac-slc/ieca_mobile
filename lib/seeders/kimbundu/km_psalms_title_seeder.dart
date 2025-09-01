f1a8412b02c452e90042867a3e96136c3d30140c
sedrac-slc <slcsedrac@gmail.com>
2025-09-01 16:54:35 +0100

    add psalms KmPsalmsContent

--- lib/seeders/kimbundu/km_psalms_title_seeder.dart
+++ lib/seeders/kimbundu/km_psalms_title_seeder.dart
@@ -2,21 +2,21 @@ import 'package:ieca_mobile/models/_import.dart';
 import 'package:ieca_mobile/seeders/_import.dart';
 
 class KmPsalmsTitleSeeder{
-  static final MIASO_19 = PsalmsTitle(id: 31, name: "JISÁLAMO 19", position: 1, lang: LanguageSectionSeeder.COKWE);
-  static final MIASO_24 = PsalmsTitle(id: 32, name: "JISÁLAMO 24", position: 2, lang: LanguageSectionSeeder.COKWE);
-  static final MIASO_46 = PsalmsTitle(id: 33, name: "JISÁLAMO 46", position: 5, lang: LanguageSectionSeeder.COKWE);
-  static final MIASO_67 = PsalmsTitle(id: 34, name: "JISÁLAMO 67", position: 6, lang: LanguageSectionSeeder.COKWE);
-  static final MIASO_96 = PsalmsTitle(id: 35, name: "JISÁLAMO 96", position: 8, lang: LanguageSectionSeeder.COKWE);
-  static final MIASO_100 = PsalmsTitle(id: 36, name: "JISÁLAMO 100", position: 9, lang: LanguageSectionSeeder.COKWE);
+  static final JISALAMO_19 = PsalmsTitle(id: 31, name: "JISÁLAMO 19", position: 1, lang: LanguageSectionSeeder.KIMBUNDU);
+  static final JISALAMO_24 = PsalmsTitle(id: 32, name: "JISÁLAMO 24", position: 2, lang: LanguageSectionSeeder.KIMBUNDU);
+  static final JISALAMO_46 = PsalmsTitle(id: 33, name: "JISÁLAMO 46", position: 5, lang: LanguageSectionSeeder.KIMBUNDU);
+  static final JISALAMO_67 = PsalmsTitle(id: 34, name: "JISÁLAMO 67", position: 6, lang: LanguageSectionSeeder.KIMBUNDU);
+  static final JISALAMO_96 = PsalmsTitle(id: 35, name: "JISÁLAMO 96", position: 8, lang: LanguageSectionSeeder.KIMBUNDU);
+  static final JISALAMO_100 = PsalmsTitle(id: 36, name: "JISÁLAMO 100", position: 9, lang: LanguageSectionSeeder.KIMBUNDU);
 
   static List<PsalmsTitle> items() {
     return [
-      MIASO_19,
-      MIASO_24,
-      MIASO_46,
-      MIASO_67,
-      MIASO_96,
-      MIASO_100,
+      JISALAMO_19,
+      JISALAMO_24,
+      JISALAMO_46,
+      JISALAMO_67,
+      JISALAMO_96,
+      JISALAMO_100,
     ];
   }
 }

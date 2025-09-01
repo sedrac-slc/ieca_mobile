7cdeb53c957784e0289fd8e9eaa1c441f01b2ef9
sedrac-slc <slcsedrac@gmail.com>
2025-09-01 11:46:29 +0100

    finish kinbumdu

--- lib/seeders/kimbundu/km_hymns_number_seeder.dart
+++ lib/seeders/kimbundu/km_hymns_number_seeder.dart
@@ -9,7 +9,7 @@ class KmHymnsNumberSeeder {
   static final GENERAL_4 = HymnsNumber(id: 913, num: 4, hymnsGroup: KmHymnsGroupSeeder.GENERAL, label: "Música: Port. 21; H.C.c/M 578", code: HymnBookCode.HC_M);
   static final GENERAL_5 = HymnsNumber(id: 914, num: 5, hymnsGroup: KmHymnsGroupSeeder.GENERAL, label: "Música: Port. 7; C.C.135", code: HymnBookCode.CC);
   static final GENERAL_6 = HymnsNumber(id: 915, num: 6, hymnsGroup: KmHymnsGroupSeeder.GENERAL, label: "Música: Port. 66; C.C. 282", code: HymnBookCode.CC);
-  static final GENERAL_7 = HymnsNumber(id: 916, num: 7, hymnsGroup: KmHymnsGroupSeeder.GENERAL, label: "Música: Port. 24;Lyra 24", code: HymnBookCode.CC);
+  static final GENERAL_7 = HymnsNumber(id: 916, num: 7, hymnsGroup: KmHymnsGroupSeeder.GENERAL, label: "Música: Port. 24;Lyra 24", code: HymnBookCode.LYARA);
   static final GENERAL_8 = HymnsNumber(id: 917, num: 8, hymnsGroup: KmHymnsGroupSeeder.GENERAL, label: "Música: Port. 288; C.C.194", code: HymnBookCode.CC);
   static final GENERAL_9 = HymnsNumber(id: 918, num: 9, hymnsGroup: KmHymnsGroupSeeder.GENERAL, label: "Música: Port. 32; H.C.c/M 500", code: HymnBookCode.HC_M);
   static final GENERAL_10 = HymnsNumber(id: 919, num: 10, hymnsGroup: KmHymnsGroupSeeder.GENERAL, label: "Música: Port. 161; C.C.62", code: HymnBookCode.CC);
@@ -19,38 +19,40 @@ class KmHymnsNumberSeeder {
   static final GENERAL_14 = HymnsNumber(id: 923, num: 14, hymnsGroup: KmHymnsGroupSeeder.GENERAL, label: "Música: Port. 247; C.C.365", code: HymnBookCode.CC);
   static final GENERAL_15 = HymnsNumber(id: 924, num: 15, hymnsGroup: KmHymnsGroupSeeder.GENERAL, label: "Música: Port. 167; C.C.200", code: HymnBookCode.CC);
   static final GENERAL_16 = HymnsNumber(id: 925, num: 16, hymnsGroup: KmHymnsGroupSeeder.GENERAL, label: "Música: Port. 17; C.C.132", code: HymnBookCode.CC);
-  static final GENERAL_17 = HymnsNumber(id: 926, num: 17, hymnsGroup: KmHymnsGroupSeeder.GENERAL, label: "", code: HymnBookCode.CC);
-  static final GENERAL_18 = HymnsNumber(id: 927, num: 18, hymnsGroup: KmHymnsGroupSeeder.GENERAL, label: "", code: HymnBookCode.CC);
-  static final GENERAL_19 = HymnsNumber(id: 928, num: 19, hymnsGroup: KmHymnsGroupSeeder.GENERAL, label: "", code: HymnBookCode.CC);
-  static final GENERAL_20 = HymnsNumber(id: 929, num: 20, hymnsGroup: KmHymnsGroupSeeder.GENERAL, label: "", code: HymnBookCode.CC);
-  static final GENERAL_21 = HymnsNumber(id: 930, num: 21, hymnsGroup: KmHymnsGroupSeeder.GENERAL, label: "", code: HymnBookCode.CC);
-  static final GENERAL_22 = HymnsNumber(id: 931, num: 22, hymnsGroup: KmHymnsGroupSeeder.GENERAL, label: "", code: HymnBookCode.CC);
-  static final GENERAL_23 = HymnsNumber(id: 932, num: 23, hymnsGroup: KmHymnsGroupSeeder.GENERAL, label: "", code: HymnBookCode.CC);
-  static final GENERAL_24 = HymnsNumber(id: 933, num: 24, hymnsGroup: KmHymnsGroupSeeder.GENERAL, label: "", code: HymnBookCode.CC);
-  static final GENERAL_25 = HymnsNumber(id: 934, num: 25, hymnsGroup: KmHymnsGroupSeeder.GENERAL, label: "", code: HymnBookCode.CC);
-  static final GENERAL_26 = HymnsNumber(id: 935, num: 26, hymnsGroup: KmHymnsGroupSeeder.GENERAL, label: "", code: HymnBookCode.CC);
-  static final GENERAL_27 = HymnsNumber(id: 936, num: 27, hymnsGroup: KmHymnsGroupSeeder.GENERAL, label: "", code: HymnBookCode.CC);
-  static final GENERAL_28 = HymnsNumber(id: 937, num: 28, hymnsGroup: KmHymnsGroupSeeder.GENERAL, label: "", code: HymnBookCode.CC);
-  static final GENERAL_29 = HymnsNumber(id: 938, num: 29, hymnsGroup: KmHymnsGroupSeeder.GENERAL, label: "", code: HymnBookCode.CC);
-  static final GENERAL_30 = HymnsNumber(id: 939, num: 30, hymnsGroup: KmHymnsGroupSeeder.GENERAL, label: "", code: HymnBookCode.CC);
-  static final GENERAL_31 = HymnsNumber(id: 940, num: 31, hymnsGroup: KmHymnsGroupSeeder.GENERAL, label: "", code: HymnBookCode.CC);
-  static final GENERAL_32 = HymnsNumber(id: 941, num: 32, hymnsGroup: KmHymnsGroupSeeder.GENERAL, label: "", code: HymnBookCode.CC);
-  static final GENERAL_33 = HymnsNumber(id: 942, num: 33, hymnsGroup: KmHymnsGroupSeeder.GENERAL, label: "", code: HymnBookCode.CC);
-  static final GENERAL_34 = HymnsNumber(id: 943, num: 34, hymnsGroup: KmHymnsGroupSeeder.GENERAL, label: "", code: HymnBookCode.CC);
-  static final GENERAL_35 = HymnsNumber(id: 944, num: 35, hymnsGroup: KmHymnsGroupSeeder.GENERAL, label: "", code: HymnBookCode.CC);
-  static final GENERAL_36 = HymnsNumber(id: 945, num: 36, hymnsGroup: KmHymnsGroupSeeder.GENERAL, label: "", code: HymnBookCode.CC);
-  static final GENERAL_37 = HymnsNumber(id: 946, num: 37, hymnsGroup: KmHymnsGroupSeeder.GENERAL, label: "", code: HymnBookCode.CC);
-  static final GENERAL_38 = HymnsNumber(id: 947, num: 38, hymnsGroup: KmHymnsGroupSeeder.GENERAL, label: "", code: HymnBookCode.CC);
-  static final GENERAL_39 = HymnsNumber(id: 948, num: 39, hymnsGroup: KmHymnsGroupSeeder.GENERAL, label: "", code: HymnBookCode.CC);
-  static final GENERAL_40 = HymnsNumber(id: 949, num: 40, hymnsGroup: KmHymnsGroupSeeder.GENERAL, label: "", code: HymnBookCode.CC);
-  static final GENERAL_41 = HymnsNumber(id: 950, num: 41, hymnsGroup: KmHymnsGroupSeeder.GENERAL, label: "", code: HymnBookCode.CC);
-  static final GENERAL_42 = HymnsNumber(id: 951, num: 42, hymnsGroup: KmHymnsGroupSeeder.GENERAL, label: "", code: HymnBookCode.CC);
-  static final GENERAL_43 = HymnsNumber(id: 952, num: 43, hymnsGroup: KmHymnsGroupSeeder.GENERAL, label: "", code: HymnBookCode.CC);
-  static final GENERAL_44 = HymnsNumber(id: 953, num: 44, hymnsGroup: KmHymnsGroupSeeder.GENERAL, label: "", code: HymnBookCode.CC);
-  static final GENERAL_45 = HymnsNumber(id: 954, num: 45, hymnsGroup: KmHymnsGroupSeeder.GENERAL, label: "", code: HymnBookCode.CC);
-  static final GENERAL_46 = HymnsNumber(id: 955, num: 46, hymnsGroup: KmHymnsGroupSeeder.GENERAL, label: "", code: HymnBookCode.CC);
-  static final GENERAL_47 = HymnsNumber(id: 956, num: 47, hymnsGroup: KmHymnsGroupSeeder.GENERAL, label: "", code: HymnBookCode.CC);
-  static final GENERAL_48 = HymnsNumber(id: 957, num: 48, hymnsGroup: KmHymnsGroupSeeder.GENERAL, label: "", code: HymnBookCode.CC);
+  static final GENERAL_17 = HymnsNumber(id: 926, num: 17, hymnsGroup: KmHymnsGroupSeeder.GENERAL, label: "Música: Port. 181; C.C.273", code: HymnBookCode.CC);
+  static final GENERAL_18 = HymnsNumber(id: 927, num: 18, hymnsGroup: KmHymnsGroupSeeder.GENERAL, label: "Música: Port. 300; C.C.381", code: HymnBookCode.CC);
+  static final GENERAL_19 = HymnsNumber(id: 928, num: 19, hymnsGroup: KmHymnsGroupSeeder.GENERAL, label: "Música: Port. 249; C.C.155", code: HymnBookCode.CC);
+  static final GENERAL_20 = HymnsNumber(id: 929, num: 20, hymnsGroup: KmHymnsGroupSeeder.GENERAL, label: "Música: Port. 158; C.C.345", code: HymnBookCode.CC);
+  static final GENERAL_21 = HymnsNumber(id: 930, num: 21, hymnsGroup: KmHymnsGroupSeeder.GENERAL, label: "Música: Port. 198; C.C.291", code: HymnBookCode.CC);
+  static final GENERAL_22 = HymnsNumber(id: 931, num: 22, hymnsGroup: KmHymnsGroupSeeder.GENERAL, label: "Música: Port. 168; C.C.336", code: HymnBookCode.CC);
+  static final GENERAL_23 = HymnsNumber(id: 932, num: 23, hymnsGroup: KmHymnsGroupSeeder.GENERAL, label: "Música: Port. 116; C.C.314", code: HymnBookCode.CC);
+  static final GENERAL_24 = HymnsNumber(id: 933, num: 24, hymnsGroup: KmHymnsGroupSeeder.GENERAL, label: "Música: Port. 303; C.C.443", code: HymnBookCode.CC);
+  static final GENERAL_25 = HymnsNumber(id: 934, num: 25, hymnsGroup: KmHymnsGroupSeeder.GENERAL, label: "Música: Port. 174; C.C.186", code: HymnBookCode.CC);
+  static final GENERAL_26 = HymnsNumber(id: 935, num: 26, hymnsGroup: KmHymnsGroupSeeder.GENERAL, label: "Música: Port. 127; C.C.525", code: HymnBookCode.CC);
+  static final GENERAL_27 = HymnsNumber(id: 936, num: 27, hymnsGroup: KmHymnsGroupSeeder.GENERAL, label: "Música: Port. 281; H.C.c/M.371", code: HymnBookCode.HC_M);
+  static final GENERAL_28 = HymnsNumber(id: 937, num: 28, hymnsGroup: KmHymnsGroupSeeder.GENERAL, label: "Música: Port. 227; C.Sr.343", code: HymnBookCode.CSr);
+  static final GENERAL_29 = HymnsNumber(id: 938, num: 29, hymnsGroup: KmHymnsGroupSeeder.GENERAL, label: "Música: Port. 294; Lyra.231", code: HymnBookCode.CC);
+  static final GENERAL_30 = HymnsNumber(id: 939, num: 30, hymnsGroup: KmHymnsGroupSeeder.GENERAL, label: "Música: Port. 295; C.C.429; HPC.86", code: HymnBookCode.CC);
+  static final GENERAL_31 = HymnsNumber(id: 940, num: 31, hymnsGroup: KmHymnsGroupSeeder.GENERAL, label: "Música: Port. 292; Lyra 229;C.C.461", code: HymnBookCode.CC);
+  static final GENERAL_32 = HymnsNumber(id: 941, num: 32, hymnsGroup: KmHymnsGroupSeeder.GENERAL, label: "Música: Port. 52; C.C.27", code: HymnBookCode.CC);
+  static final GENERAL_33 = HymnsNumber(id: 942, num: 33, hymnsGroup: KmHymnsGroupSeeder.GENERAL, label: "Música: Port. 33; Lyra 40", code: HymnBookCode.CC);
+  static final GENERAL_34 = HymnsNumber(id: 943, num: 34, hymnsGroup: KmHymnsGroupSeeder.GENERAL, label: "Música: Port. 47; H.C.c/M.89;S.H.316", code: HymnBookCode.HC_M);
+  static final GENERAL_35 = HymnsNumber(id: 944, num: 35, hymnsGroup: KmHymnsGroupSeeder.GENERAL, label: "Música: Port. 50; C.C.26", code: HymnBookCode.CC);
+  static final GENERAL_36 = HymnsNumber(id: 945, num: 36, hymnsGroup: KmHymnsGroupSeeder.GENERAL, label: "Música: Port. 172; A.C.4", code: HymnBookCode.AC);
+  static final GENERAL_37 = HymnsNumber(id: 946, num: 37, hymnsGroup: KmHymnsGroupSeeder.GENERAL, label: "Música: Port. 297; C.C.289", code: HymnBookCode.CC);
+  static final GENERAL_38 = HymnsNumber(id: 947, num: 38, hymnsGroup: KmHymnsGroupSeeder.GENERAL, label: "Música: Port. 115; C.C.93", code: HymnBookCode.CC);
+  static final GENERAL_39 = HymnsNumber(id: 948, num: 39, hymnsGroup: KmHymnsGroupSeeder.GENERAL, label: "Música: Port. 77; Lyra 53", code: HymnBookCode.LYARA);
+  static final GENERAL_40 = HymnsNumber(id: 949, num: 40, hymnsGroup: KmHymnsGroupSeeder.GENERAL, label: "Música: Port. 81; C.C.99", code: HymnBookCode.CC);
+  static final GENERAL_41 = HymnsNumber(id: 950, num: 41, hymnsGroup: KmHymnsGroupSeeder.GENERAL, label: "Música: Port. 311; Lyra 240", code: HymnBookCode.LYARA);
+  static final GENERAL_42 = HymnsNumber(id: 951, num: 42, hymnsGroup: KmHymnsGroupSeeder.GENERAL, label: "Música: Port. 78; Lyra 49; C.C. 402", code: HymnBookCode.LYARA);
+  static final GENERAL_43 = HymnsNumber(id: 952, num: 43, hymnsGroup: KmHymnsGroupSeeder.GENERAL, label: "Música: Port. 277; C.C.481", code: HymnBookCode.CC);
+  static final GENERAL_44 = HymnsNumber(id: 953, num: 44, hymnsGroup: KmHymnsGroupSeeder.GENERAL, label: "Música: Port. 250; C.C.343", code: HymnBookCode.CC);
+  static final GENERAL_45 = HymnsNumber(id: 954, num: 45, hymnsGroup: KmHymnsGroupSeeder.GENERAL, label: "Música: Port. 341; Lyra 297; C.C.111", code: HymnBookCode.LYARA);
+  static final GENERAL_46 = HymnsNumber(id: 955, num: 46, hymnsGroup: KmHymnsGroupSeeder.GENERAL, label: "Música: Port. 106; Lyra 69; C.C.137", code: HymnBookCode.LYARA);
+  static final GENERAL_47 = HymnsNumber(id: 956, num: 47, hymnsGroup: KmHymnsGroupSeeder.GENERAL, label: "Música: Port. 316; Lyara 251", code: HymnBookCode.LYARA);
+  static final GENERAL_48 = HymnsNumber(id: 957, num: 48, hymnsGroup: KmHymnsGroupSeeder.GENERAL, label: "Música: Port. 75; C.C. 35", code: HymnBookCode.CC);
+  static final GENERAL_49 = HymnsNumber(id: 958, num: 49, hymnsGroup: KmHymnsGroupSeeder.GENERAL, label: "Música: Port. 75; C.C. 312", code: HymnBookCode.CC);
+
 
   static List<HymnsNumber> items() {
     return [
@@ -60,6 +62,7 @@ class KmHymnsNumberSeeder {
       GENERAL_25, GENERAL_26, GENERAL_27, GENERAL_28, GENERAL_29, GENERAL_30, GENERAL_31, GENERAL_32,
       GENERAL_33, GENERAL_34, GENERAL_35, GENERAL_36, GENERAL_37, GENERAL_38, GENERAL_39, GENERAL_40,
       GENERAL_41, GENERAL_42, GENERAL_43, GENERAL_44, GENERAL_45, GENERAL_46, GENERAL_47, GENERAL_48,
+      GENERAL_49,
     ];
   }
 }
--- lib/seeders/kimbundu/km_psalms_title_seeder.dart
+++ lib/seeders/kimbundu/km_psalms_title_seeder.dart
@@ -2,12 +2,12 @@ import 'package:ieca_mobile/models/_import.dart';
 import 'package:ieca_mobile/seeders/_import.dart';
 
 class KmPsalmsTitleSeeder{
-  static final MIASO_19 = PsalmsTitle(id: 31, name: "MIASO 19", position: 1, lang: LanguageSectionSeeder.COKWE);
-  static final MIASO_24 = PsalmsTitle(id: 32, name: "MIASO 24", position: 2, lang: LanguageSectionSeeder.COKWE);
-  static final MIASO_46 = PsalmsTitle(id: 33, name: "MIASO 46", position: 5, lang: LanguageSectionSeeder.COKWE);
-  static final MIASO_67 = PsalmsTitle(id: 34, name: "MIASO 67", position: 6, lang: LanguageSectionSeeder.COKWE);
-  static final MIASO_96 = PsalmsTitle(id: 35, name: "MIASO 96", position: 8, lang: LanguageSectionSeeder.COKWE);
-  static final MIASO_100 = PsalmsTitle(id: 36, name: "MIASO 100", position: 9, lang: LanguageSectionSeeder.COKWE);
+  static final MIASO_19 = PsalmsTitle(id: 31, name: "JISÁLAMO 19", position: 1, lang: LanguageSectionSeeder.COKWE);
+  static final MIASO_24 = PsalmsTitle(id: 32, name: "JISÁLAMO 24", position: 2, lang: LanguageSectionSeeder.COKWE);
+  static final MIASO_46 = PsalmsTitle(id: 33, name: "JISÁLAMO 46", position: 5, lang: LanguageSectionSeeder.COKWE);
+  static final MIASO_67 = PsalmsTitle(id: 34, name: "JISÁLAMO 67", position: 6, lang: LanguageSectionSeeder.COKWE);
+  static final MIASO_96 = PsalmsTitle(id: 35, name: "JISÁLAMO 96", position: 8, lang: LanguageSectionSeeder.COKWE);
+  static final MIASO_100 = PsalmsTitle(id: 36, name: "JISÁLAMO 100", position: 9, lang: LanguageSectionSeeder.COKWE);
 
   static List<PsalmsTitle> items() {
     return [

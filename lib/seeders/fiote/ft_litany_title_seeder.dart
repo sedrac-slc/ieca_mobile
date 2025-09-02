ae457256921bd5c6eeed8432b787c9de4e680169
sedrac-slc <slcsedrac@gmail.com>
2025-09-02 15:01:10 +0100

    insert litany for fiote

--- lib/seeders/fiote/ft_litany_title_seeder.dart
+++ lib/seeders/fiote/ft_litany_title_seeder.dart
@@ -2,11 +2,17 @@ import 'package:ieca_mobile/models/_import.dart';
 import 'package:ieca_mobile/seeders/_import.dart';
 
 class FtLitanyTitleSeeder{
-  //NSAMBU WA NYA – MBUTUKULU A YESU
+
+  static final NDOMBO_A_NSAMBUDULU = LitanyTitle(id: 30, name: "Nsambu Wantete – Ndombo A Nsambudulu", position: 1, lang: LanguageSectionSeeder.FIOTE);
+  static final NGIA_MBUDULU_A_MASUMU = LitanyTitle(id: 31, name: "Nsambu Wanzole - Ngia Mbudulu A Masumu", position: 2, lang: LanguageSectionSeeder.FIOTE);
+  static final MVUTUDULU_A_MATONDO = LitanyTitle(id: 32, name: "Nsambu Wa Ntantu - Mvutudulu A Matondo", position: 3, lang: LanguageSectionSeeder.FIOTE);
+  static final MBUTUKULU_A_YESU = LitanyTitle(id: 33, name: "Nsambu Wa Nya – Mbutukulu A Yesu", position: 4, lang: LanguageSectionSeeder.FIOTE);
+  static final MFULUKULU_A_YESU = LitanyTitle(id: 34, name: "Nsambu Wa Ntanu – Mfulukulu A Yesu", position: 5, lang: LanguageSectionSeeder.FIOTE);
 
   static List<LitanyTitle> items() {
     return [
-
+      NDOMBO_A_NSAMBUDULU, NGIA_MBUDULU_A_MASUMU, MVUTUDULU_A_MATONDO,
+      MBUTUKULU_A_YESU, MFULUKULU_A_YESU,
     ];
   }
 }
